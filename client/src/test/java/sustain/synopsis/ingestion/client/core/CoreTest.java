a828d63079f48ea79f5f8983da066c05a82f77fd
Thilina Buddhika <thilinamb@gmail.com>
2019-10-09 19:25:32 -0600

    Updated unit tests with the API changes in the temporal quantizer.

--- client/src/test/java/sustain/synopsis/ingestion/client/core/CoreTest.java
+++ client/src/test/java/sustain/synopsis/ingestion/client/core/CoreTest.java
@@ -1,51 +1,54 @@
 package sustain.synopsis.ingestion.client.core;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import sustain.synopsis.sketch.dataset.feature.Feature;
 import sustain.synopsis.sketch.graph.DataContainer;
 import sustain.synopsis.sketch.graph.Path;
+import sustain.synopsis.sketch.serialization.SerializationException;
+import sustain.synopsis.sketch.serialization.SerializationInputStream;
+import sustain.synopsis.sketch.serialization.SerializationOutputStream;
 import sustain.synopsis.sketch.stat.RunningStatisticsND;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.Month;
-import java.time.temporal.Temporal;
 
-public class CoreTest {
+class CoreTest {
 
-    private Strand createStrand(Path path, String geohash, long ts, double ...features){
+    private Strand createStrand(Path path, String geohash, long ts, long to, double... features) {
         path.add(new Feature("location", geohash));
         path.add(new Feature("time", ts));
-        StringBuilder keyBuilder = new StringBuilder();
-        keyBuilder.append(geohash).append(ts);
-        for (int i = 0; i< features.length; i++){
+        for (int i = 0; i < features.length; i++) {
             path.add(new Feature("feature_" + (i + 1), features[i]));
-            keyBuilder.append(features[i]);
         }
         RunningStatisticsND runningStats = new RunningStatisticsND(features);
         DataContainer container = new DataContainer(runningStats);
-        path.get(path.size()-1).setData(container);
-        return new Strand("9xj", 1391216400000L, path, keyBuilder.toString());
+        path.get(path.size() - 1).setData(container);
+        return new Strand("9xj", ts, to, path);
     }
 
     @Test
-    void testStrandInitialization(){
+    void testStrandInitialization() {
         Path path = new Path(4);
-        Strand strand = createStrand(path, "9xj", 1391216400000L, 1.34, 1.5);
+        Strand strand = createStrand(path, "9xj", (1391216400000L - 3600 * 1000), 1391216400000L, 1.34, 1.5);
 
         Assertions.assertEquals("9xj", strand.getGeohash());
-        Assertions.assertEquals(1391216400000L, strand.getTimestamp());
+        Assertions.assertEquals((1391216400000L - 3600 * 1000), strand.getFromTimeStamp());
+        Assertions.assertEquals(1391216400000L, strand.getToTimestamp());
         Assertions.assertEquals(path, strand.getPath());
     }
 
     @Test
-    void testStrandMerge(){
+    void testStrandMerge() {
         Path path1 = new Path(4);
-        Strand strand1 = createStrand(path1,"9xj", 1391216400000L, 1.34, 1.5);
+        Strand strand1 = createStrand(path1, "9xj", (1391216400000L - 3600 * 1000), 1391216400000L,1.34, 1.5);
 
         Path path2 = new Path(4);
-        Strand strand2 = createStrand(path2,"9xj", 1391216400000L, 1.34, 1.5);
+        Strand strand2 = createStrand(path2, "9xj", (1391216400000L - 3600 * 1000), 1391216400000L,1.34, 1.5);
 
         strand1.merge(strand2);
 
@@ -54,52 +57,98 @@ public class CoreTest {
     }
 
     @Test
-    void testStrandMergeExceptions(){
-        Path path1 = new Path(4);
-        Strand strand1 = createStrand(path1,"9xj", 1391216400000L, 1.34, 1.5);
+    void testStrandEquals(){
+        Path path = new Path(4);
+        Strand strand = createStrand(path, "9xj", (1391216400000L - 3600 * 1000), 1391216400000L,1.34, 1.5);
 
-        // different path lengths
-        Path path2 = new Path(3);
-        final Strand strand2 = createStrand(path2, "9xj", 1391216400000L, 1.34);
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {strand1.merge(strand2);});
+        Strand strand2 = new Strand("9xj", (1391216400000L - 3600 * 1000), 1391216400000L, path);
+        Assertions.assertEquals(strand, strand2);
 
-        // different paths
-        path2 = new Path(4);
-        final Strand strand3 = createStrand(path2, "9xj", 1391216400000L, 1.34, 1.6);
-        Assertions.assertThrows(IllegalArgumentException.class, ()->{strand1.merge(strand3);});
+        // different geohash
+        Strand strand3 = new Strand("9xi", (1391216400000L - 3600 * 1000), 1391216400000L, path);
+        Assertions.assertNotEquals(strand, strand3);
+
+        // different from timestamp
+        Strand strand4 = new Strand("9xj", (1391216300000L - 3600 * 1000), 1391216400000L, path);
+        Assertions.assertNotEquals(strand, strand4);
+
+        // different to timestamp
+        Strand strand5 = new Strand("9xj", (1391216400000L - 3600 * 1000), 1391216500000L, path);
+        Assertions.assertNotEquals(strand, strand5);
+
+        // different path
+        Path path2 = new Path(5);
+        Strand strand6 = createStrand(path, "9xj", (1391216400000L - 3600 * 1000), 1391216400000L,1.34, 1.5, 4.6);
+        Assertions.assertNotEquals(strand, strand6);
+
+        // different metadata
+        strand2.addMetadata("key1", "value1");
+        Assertions.assertNotEquals(strand, strand2);
     }
 
     @Test
-    void testTemporalQuantizer(){
+    void testStrandSerialization() throws IOException, SerializationException {
+        Path path = new Path(4);
+        Strand strand = createStrand(path, "9xj", (1391216400000L - 3600 * 1000), 1391216400000L,1.34, 1.5);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        SerializationOutputStream sos = new SerializationOutputStream(baos);
+        strand.serialize(sos);
+        sos.flush();
+        baos.flush();
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        SerializationInputStream sis = new SerializationInputStream(bais);
+        Strand deserializedStrand = new Strand(sis);
+        Assertions.assertEquals(strand, deserializedStrand);
+    }
+
+    @Test
+    void testStrandMergeExceptions() {
+        Path path1 = new Path(4);
+        Strand strand1 = createStrand(path1, "9xj", (1391216400000L - 3600 * 1000), 1391216400000L,1.34, 1.5);
+
+        // different path lengths
+        Path path2 = new Path(3);
+        final Strand strand2 = createStrand(path2, "9xj", (1391216400000L - 3600 * 1000), 1391216400000L,1.34);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            strand1.merge(strand2);
+        });
+
+        // different paths
+        path2 = new Path(4);
+        final Strand strand3 = createStrand(path2, "9xj", (1391216400000L - 3600 * 1000), 1391216400000L,1.34, 1.6);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            strand1.merge(strand3);
+        });
+    }
+
+    @Test
+    void testTemporalQuantizer() {
         TemporalQuantizer quantizer = new TemporalQuantizer(Duration.ofHours(1));
         LocalDateTime ts1 = LocalDateTime.of(2019, Month.JANUARY, 12, 6, 21, 30);
-        long returned = quantizer.getBoundary( quantizer.getBoundary(TemporalQuantizer.localDateTimeToEpoch(ts1)));
-        long expectedBoundary = TemporalQuantizer.localDateTimeToEpoch(LocalDateTime.of(2019, Month.JANUARY,
-                12, 7, 0, 0));
-        Assertions.assertEquals(expectedBoundary, returned);
+        long[] returned = quantizer.getTemporalBoundaries(TemporalQuantizer.localDateTimeToEpoch(ts1));
+        long expectedUpperBoundary = TemporalQuantizer.localDateTimeToEpoch(LocalDateTime.of(2019, Month.JANUARY, 12,
+                7, 0, 0));
+        long expectedLowerBoundary = TemporalQuantizer.localDateTimeToEpoch(LocalDateTime.of(2019, Month.JANUARY, 12,
+                6, 0, 0));
+        Assertions.assertEquals(expectedLowerBoundary, returned[0]);
+        Assertions.assertEquals(expectedUpperBoundary, returned[1]);
 
         // simulate an out of order record - now the boundary is advanced to 07:00
         ts1 = LocalDateTime.of(2019, Month.JANUARY, 12, 3, 21, 30);
-        returned = quantizer.getBoundary( quantizer.getBoundary(TemporalQuantizer.localDateTimeToEpoch(ts1)));
-        expectedBoundary = TemporalQuantizer.localDateTimeToEpoch(LocalDateTime.of(2019, Month.JANUARY,
-                12, 4, 0, 0));
-        System.out.println(TemporalQuantizer.epochToLocalDateTime(returned));
-        Assertions.assertEquals(expectedBoundary, returned);
+        returned = quantizer.getTemporalBoundaries(TemporalQuantizer.localDateTimeToEpoch(ts1));
+        expectedUpperBoundary = TemporalQuantizer.localDateTimeToEpoch(LocalDateTime.of(2019, Month.JANUARY, 12, 4, 0, 0));
+        Assertions.assertEquals(expectedUpperBoundary, returned[1]);
 
         // simulate a sparse observation
         ts1 = LocalDateTime.of(2019, Month.JANUARY, 12, 17, 21, 30);
-        returned = quantizer.getBoundary( quantizer.getBoundary(TemporalQuantizer.localDateTimeToEpoch(ts1)));
-        expectedBoundary = TemporalQuantizer.localDateTimeToEpoch(LocalDateTime.of(2019, Month.JANUARY,
-                12, 18, 0, 0));
-        Assertions.assertEquals(expectedBoundary, returned);
+        returned = quantizer.getTemporalBoundaries(TemporalQuantizer.localDateTimeToEpoch(ts1));
+        expectedUpperBoundary = TemporalQuantizer.localDateTimeToEpoch(LocalDateTime.of(2019, Month.JANUARY, 12, 18, 0, 0));
+        Assertions.assertEquals(expectedUpperBoundary, returned[1]);
 
-        // check the boundaries
-
+        // check when the ts = boundaries
         ts1 = LocalDateTime.of(2019, Month.JANUARY, 12, 18, 0, 0);
-        returned = quantizer.getBoundary( quantizer.getBoundary(TemporalQuantizer.localDateTimeToEpoch(ts1)));
-        expectedBoundary = TemporalQuantizer.localDateTimeToEpoch(LocalDateTime.of(2019, Month.JANUARY,
-                12, 18, 0, 0));
-        System.out.println(TemporalQuantizer.epochToLocalDateTime(returned));
-        Assertions.assertEquals(expectedBoundary, returned);
+        returned = quantizer.getTemporalBoundaries(TemporalQuantizer.localDateTimeToEpoch(ts1));
+        expectedUpperBoundary = TemporalQuantizer.localDateTimeToEpoch(LocalDateTime.of(2019, Month.JANUARY, 12, 18, 0, 0));
+        Assertions.assertEquals(expectedUpperBoundary, returned[1]);
     }
 }

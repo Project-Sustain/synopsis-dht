5f0675570ae545831540b3295c8a85626fef9a06
Thilina Buddhika <thilinamb@gmail.com>
2019-12-12 19:01:26 -0700

    Refactoring: package rename

--- common/src/main/java/sustain/synopsis/common/kafka/Publisher.java
+++ common/src/main/java/sustain/synopsis/common/kafka/Publisher.java
@@ -1,4 +1,4 @@
-package synopsis2.common.kafka;
+package sustain.synopsis.common.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;

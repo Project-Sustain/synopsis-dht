46330068b88b26377159c04c6e85feeb558b5bca
Thilina Buddhika <thilinamb@gmail.com>
2019-12-20 14:05:50 -0700

    serialization support for ingestion session

--- dht/src/main/java/sustain/synopsis/dht/store/IngestionSession.java
+++ dht/src/main/java/sustain/synopsis/dht/store/IngestionSession.java
@@ -2,6 +2,9 @@ package sustain.synopsis.dht.store;
 
 import sustain.synopsis.storage.lsmtree.Metadata;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -21,11 +24,11 @@ public class IngestionSession implements Comparable<IngestionSession> {
         this.serializedSSTables = new ArrayList<>();
     }
 
-    public void setComplete(){
+    public void setComplete() {
         this.complete = true;
     }
 
-    public void addSerializedSSTable(Metadata<StrandStorageKey> metadata){
+    public void addSerializedSSTable(Metadata<StrandStorageKey> metadata) {
         this.serializedSSTables.add(metadata);
     }
 
@@ -45,5 +48,55 @@ public class IngestionSession implements Comparable<IngestionSession> {
     @Override
     public int compareTo(IngestionSession o) {
         return Long.compare(this.sessionId, o.sessionId);
+    }
+
+    public void serialize(DataOutputStream dos) throws IOException {
+        dos.writeUTF(ingestionUser);
+        dos.writeLong(ingestionTime);
+        dos.writeLong(sessionId);
+        dos.writeBoolean(complete);
+        dos.writeInt(serializedSSTables.size());
+        for (Metadata<StrandStorageKey> metadata : serializedSSTables) {
+            metadata.serialize(dos);
+        }
+    }
+
+    public static IngestionSession deserialize(DataInputStream dis) throws IOException, InstantiationException,
+            IllegalAccessException {
+        String ingestionUser = dis.readUTF();
+        long ingestionTime = dis.readLong();
+        long sessionId = dis.readLong();
+
+        IngestionSession ingestionSession = new IngestionSession(ingestionUser, ingestionTime, sessionId);
+        if (dis.readBoolean()) {
+            ingestionSession.setComplete();
+        }
+        int ssTableCount = dis.readInt();
+        for (int i = 0; i < ssTableCount; i++) {
+            Metadata<StrandStorageKey> metadata = new Metadata<>();
+            metadata.deserialize(dis, StrandStorageKey.class);
+            ingestionSession.addSerializedSSTable(metadata);
+        }
+        return ingestionSession;
+    }
+
+    public String getIngestionUser() {
+        return ingestionUser;
+    }
+
+    public long getIngestionTime() {
+        return ingestionTime;
+    }
+
+    public long getSessionId() {
+        return sessionId;
+    }
+
+    public boolean isComplete() {
+        return complete;
+    }
+
+    public List<Metadata<StrandStorageKey>> getSerializedSSTables() {
+        return serializedSSTables;
     }
 }

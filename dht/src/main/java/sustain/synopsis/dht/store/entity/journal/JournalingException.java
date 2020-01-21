cf5b1a85e0c03410a58c0e6bec3dcfe0ec4f6ed1
Thilina Buddhika <thilinamb@gmail.com>
2020-01-21 12:15:57 -0700

    Create JournalingException.java

---
+++ dht/src/main/java/sustain/synopsis/dht/store/entity/journal/JournalingException.java
@@ -0,0 +1,7 @@
+package sustain.synopsis.dht.store.entity.journal;
+
+public class JournalingException extends Exception {
+    public JournalingException(String message) {
+        super(message);
+    }
+}

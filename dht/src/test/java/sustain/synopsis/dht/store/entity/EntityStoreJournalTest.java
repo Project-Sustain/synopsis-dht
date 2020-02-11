0835b0a6c3d24168a4bc6ec140dbb81bda4891c5
Thilina Buddhika <thilinamb@gmail.com>
2020-02-11 12:51:37 -0700

    Updated journaliing tests

--- dht/src/test/java/sustain/synopsis/dht/store/entity/EntityStoreJournalTest.java
+++ dht/src/test/java/sustain/synopsis/dht/store/entity/EntityStoreJournalTest.java
@@ -1,11 +1,9 @@
 package sustain.synopsis.dht.store.entity;
 
-import junit.framework.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import sustain.synopsis.dht.journal.Logger;
-import sustain.synopsis.dht.store.IngestionSession;
 import sustain.synopsis.dht.store.StorageException;
 import sustain.synopsis.dht.store.StrandStorageKey;
 import sustain.synopsis.dht.store.entity.journal.JournalingException;
@@ -17,7 +15,9 @@ import sustain.synopsis.storage.lsmtree.Metadata;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class EntityStoreJournalTest {
@@ -34,15 +34,15 @@ public class EntityStoreJournalTest {
 
     @Test
     void testValidateSerializeSSTableActivity() throws IOException {
-        Map<Long, IngestionSession> sessions = new HashMap<>();
-        sessions.put(1000L, new IngestionSession("bob", 123L, 1000L));
-        sessions.put(1001L, new IngestionSession("alice", 678L, 1001L));
+        Map<Long, List<Metadata<StrandStorageKey>>> metadataList = new HashMap<>();
+        metadataList.put(1000L, new ArrayList<>());
+        metadataList.put(1001L, new ArrayList<>());
 
         EntityStoreJournal metaStore = new EntityStoreJournal("", "");
 
         // invalid session id
         SerializeSSTableActivity activity = new SerializeSSTableActivity(1002L, new Metadata<>());
-        Assertions.assertFalse(metaStore.validateSerializeSSTableActivity(sessions, activity));
+        Assertions.assertFalse(metaStore.validateSerializeSSTableActivity(metadataList, activity));
 
         // non-existing path
         String path = tempDir.getAbsolutePath() + File.separator + "non-existing-path";
@@ -51,12 +51,12 @@ public class EntityStoreJournalTest {
         metadata.setMax(new StrandStorageKey(20L, 25L));
         metadata.setPath(path);
         activity = new SerializeSSTableActivity(1000L, metadata);
-        Assertions.assertFalse(metaStore.validateSerializeSSTableActivity(sessions, activity));
+        Assertions.assertFalse(metaStore.validateSerializeSSTableActivity(metadataList, activity));
 
         // ssTable file points to a directory
         metadata.setPath(tempDir.getAbsolutePath());
         activity = new SerializeSSTableActivity(1000L, metadata);
-        Assertions.assertFalse(metaStore.validateSerializeSSTableActivity(sessions, activity));
+        Assertions.assertFalse(metaStore.validateSerializeSSTableActivity(metadataList, activity));
 
         // valid entry
         File f = new File(tempDir.getAbsolutePath() + File.separator + "test.file");
@@ -64,13 +64,13 @@ public class EntityStoreJournalTest {
         Assertions.assertTrue(f.exists());
         metadata.setPath(f.getAbsolutePath());
         activity = new SerializeSSTableActivity(1000L, metadata);
-        Assertions.assertTrue(metaStore.validateSerializeSSTableActivity(sessions, activity));
+        Assertions.assertTrue(metaStore.validateSerializeSSTableActivity(metadataList, activity));
     }
 
     @Test
     void testValidateEndSessionActivity() {
-        Map<Long, IngestionSession> sessions = new HashMap<>();
-        sessions.put(1000L, new IngestionSession("bob", 123L, 1000L));
+        Map<Long, List<Metadata<StrandStorageKey>>> sessions = new HashMap<>();
+        sessions.put(1000L, new ArrayList<>());
 
         EntityStoreJournal metaStore = new EntityStoreJournal("", "");
         // invalid session id
@@ -83,7 +83,7 @@ public class EntityStoreJournalTest {
     void testParseJournal() throws IOException, StorageException, JournalingException {
         Logger logger = new Logger(tempDir.getAbsolutePath() + File.separator + "test.slog");
 
-        StartSessionActivity startSessionActivity = new StartSessionActivity("bob", 123L, 1000L);
+        StartSessionActivity startSessionActivity = new StartSessionActivity(1000L);
         logger.append(startSessionActivity.serialize());
 
         File f = new File(tempDir.getAbsolutePath() + File.separator + "test.file");
@@ -91,43 +91,35 @@ public class EntityStoreJournalTest {
         Metadata<StrandStorageKey> metadata1 = new Metadata<>();
         metadata1.setMin(new StrandStorageKey(10L, 15L));
         metadata1.setMax(new StrandStorageKey(20L, 25L));
-        metadata1.setPath(f.getAbsolutePath());
+        metadata1.setPath(f.getAbsolutePath()); // use a valid file path
+        metadata1.setSessionId(1000L);
+        metadata1.setUser("bob");
         SerializeSSTableActivity serializeSSTableActivity = new SerializeSSTableActivity(1000L, metadata1);
         logger.append(serializeSSTableActivity.serialize());
         logger.append(new IncSeqIdActivity(1).serialize());
 
         metadata1.setMin(new StrandStorageKey(30L, 35L));
         metadata1.setMin(new StrandStorageKey(50L, 55L));
-        metadata1.setPath(f.getAbsolutePath()); // use a valid path to pass the validation
         logger.append(serializeSSTableActivity.serialize());
 
         logger.append(new EndSessionActivity(1000L).serialize());
 
-        logger.append(new StartSessionActivity("alice", 567L, 2000L).serialize());
+        logger.append(new StartSessionActivity(2000L).serialize());
         logger.append(new EndSessionActivity(2000L).serialize());
         logger.append(new IncSeqIdActivity(2).serialize());
         logger.close();
 
         EntityStoreJournal metaStore = new EntityStoreJournal("", "");
         metaStore.parseJournal(logger.iterator());
-        Map<Long, IngestionSession> sessions = metaStore.getSessions();
-
-        Assertions.assertEquals(2, sessions.size());
-        IngestionSession session1 = sessions.get(1000L);
-        Assertions.assertNotNull(session1);
-        Assertions.assertEquals(2, session1.getSerializedSSTables().size());
-        Assertions.assertTrue(session1.isComplete());
-
-        IngestionSession session2 = sessions.get(2000L);
-        Assertions.assertNotNull(session2);
-        Assertions.assertTrue(session2.isComplete());
-        Assert.assertEquals(2, metaStore.getSequenceId());
+        List<Metadata<StrandStorageKey>> metadataList = metaStore.getMetadata();
+        Assertions.assertEquals(2, metadataList.size());
+        Assertions.assertEquals(2, metaStore.getSequenceId());
     }
 
     @Test
     void testParsingInvalidEvents() throws IOException, StorageException {
         Logger logger = new Logger(tempDir.getAbsolutePath() + File.separator + "test.slog");
-        StartSessionActivity startSessionActivity = new StartSessionActivity("bob", 123L, 1000L);
+        StartSessionActivity startSessionActivity = new StartSessionActivity(1000L);
         logger.append(startSessionActivity.serialize());
 
         File f = new File(tempDir.getAbsolutePath() + File.separator + "test.file"); // does not exist
@@ -135,6 +127,9 @@ public class EntityStoreJournalTest {
         metadata1.setMin(new StrandStorageKey(10L, 15L));
         metadata1.setMax(new StrandStorageKey(20L, 25L));
         metadata1.setPath(f.getAbsolutePath());
+        metadata1.setSessionId(1000L);
+        metadata1.setUser("bob");
+        metadata1.setSessionStartTS(System.currentTimeMillis());
         SerializeSSTableActivity serializeSSTableActivity = new SerializeSSTableActivity(1000L, metadata1);
         logger.append(serializeSSTableActivity.serialize());
 

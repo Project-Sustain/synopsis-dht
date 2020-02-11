45e3129b3ea2f9de82f6855e8a60aeba54f175cf
Thilina Buddhika <thilinamb@gmail.com>
2020-02-11 12:51:25 -0700

    Placeholder session validator

---
+++ dht/src/main/java/sustain/synopsis/dht/store/SessionValidator.java
@@ -0,0 +1,9 @@
+package sustain.synopsis.dht.store;
+
+public class SessionValidator {
+
+    public boolean validate(String userId, String dataseId, long sessionId){
+        // TODO: we need to fix this once the metadata server is ready with another gRPC service
+        return true;
+    }
+}

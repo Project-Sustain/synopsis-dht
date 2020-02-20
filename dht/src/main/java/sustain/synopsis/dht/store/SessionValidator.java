5d78c55ff0f4902020766bca01be32c6f0895dea
Thilina Buddhika <thilinamb@gmail.com>
2020-02-19 18:11:33 -0700

    Mock session validator
    This will be replaced when the session validation service of the metadata server is ready

--- dht/src/main/java/sustain/synopsis/dht/store/SessionValidator.java
+++ dht/src/main/java/sustain/synopsis/dht/store/SessionValidator.java
@@ -2,8 +2,24 @@ package sustain.synopsis.dht.store;
 
 public class SessionValidator {
 
-    public boolean validate(String userId, String dataseId, long sessionId){
+    // todo: we need to replace this with appropriate message type from the session validation gRPC service of the
+    // metadata server
+    public static class SessionValidationResponse {
+        public boolean valid;
+        // optional
+        public String userId;
+        public long sessionStartTS;
+
+        public SessionValidationResponse(boolean valid, String userId, long sessionStartTS) {
+            this.valid = valid;
+            this.userId = userId;
+            this.sessionStartTS = sessionStartTS;
+        }
+    }
+
+    public SessionValidationResponse validate(String dataseId, long sessionId) {
         // TODO: we need to fix this once the metadata server is ready with another gRPC service
-        return true;
+        SessionValidationResponse response = new SessionValidationResponse(true, "test_user",System.currentTimeMillis());
+        return response;
     }
 }

49b5f6f0fc7c294f329787d55adec931150520b1
Thilina Buddhika <thilinamb@gmail.com>
2020-02-24 20:00:35 -0700

    Reusing a serialization code of the yaml config across multiple test suits

--- dht/src/test/java/sustain/synopsis/dht/NodeConfigurationTest.java
+++ dht/src/test/java/sustain/synopsis/dht/NodeConfigurationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -17,19 +18,7 @@ public class NodeConfigurationTest {
     @Test
     void testGetters() throws IOException {
         Path nodeConfigFilePath = tempDir.resolve("test.yaml");
-        FileWriter fw = new FileWriter(nodeConfigFilePath.toFile());
-        fw.append("---\n");
-        fw.append("ingestionServicePort: 9091\n");
-        fw.append("storageDirs:\n");
-        fw.append("  /tmp/ : 1024\n");
-        fw.append("storageAllocationPolicy: 'round-robin'\n");
-        fw.append("rootJournalLoc: '/tmp/root-journal.slog'\n");
-        fw.append("memTableSize: 50\n");
-        fw.append("blockSize: 10\n");
-        fw.append("metadataStoreDir: '/tmp'\n");
-        fw.append("writerPoolSize: 5\n");
-        fw.flush();
-        fw.close();
+        serializeNodeConfig(nodeConfigFilePath.toFile());
         NodeConfiguration configuration = NodeConfiguration.fromYamlFile(nodeConfigFilePath.toAbsolutePath().toString());
 
         Assertions.assertEquals(9091, configuration.getIngestionServicePort());
@@ -60,5 +49,21 @@ public class NodeConfigurationTest {
         Assertions.assertEquals("/" + hostname + "/tmp/",
                 configuration.getStorageDirs().keySet().iterator().next());
         Assertions.assertEquals("/" + hostname + "/tmp/root-journal.slog", configuration.getRootJournalLoc());
+    }
+
+    public static void serializeNodeConfig(File output) throws IOException {
+        FileWriter fw = new FileWriter(output);
+        fw.append("---\n");
+        fw.append("ingestionServicePort: 9091\n");
+        fw.append("storageDirs:\n");
+        fw.append("  /tmp/ : 1024\n");
+        fw.append("storageAllocationPolicy: 'round-robin'\n");
+        fw.append("rootJournalLoc: '/tmp/root-journal.slog'\n");
+        fw.append("memTableSize: 50\n");
+        fw.append("blockSize: 10\n");
+        fw.append("metadataStoreDir: '/tmp'\n");
+        fw.append("writerPoolSize: 5\n");
+        fw.flush();
+        fw.close();
     }
 }

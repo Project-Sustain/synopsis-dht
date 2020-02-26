ad4e7009570e0d5337be252a7ce272486b0ddef4
Thilina Buddhika <thilinamb@gmail.com>
2020-02-26 11:10:58 -0700

    Removing unused Kafka code

--- client/src/main/java/synopsis2/client/Util.java
+++ client/src/main/java/synopsis2/client/Util.java
@@ -1,18 +1,14 @@
 package synopsis2.client;
 
-import org.apache.kafka.clients.admin.*;
-import org.apache.kafka.common.KafkaFuture;
 import org.apache.log4j.Logger;
-import sustain.synopsis.dht.Context;
-import sustain.synopsis.dht.ServerConstants;
 import sustain.synopsis.sketch.dataset.Quantizer;
 import sustain.synopsis.sketch.dataset.feature.Feature;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.ExecutionException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Util {
 
@@ -39,29 +35,5 @@ public class Util {
             throw e;
         }
         return quantizers;
-    }
-
-    public static void createKafkaTopicIfNotExists(String topic, int partitions, short replicationCount) throws Exception {
-        Properties properties = new Properties();
-        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,
-                Context.getInstance().getProperty(ServerConstants.Configuration.KAFKA_BOOTSTRAP_BROKERS));
-        AdminClient admin = AdminClient.create(properties);
-        try {
-            // get the list of existing topics
-            ListTopicsResult existingTopicsResult = admin.listTopics();
-            KafkaFuture<Set<String>> topicListFuture = existingTopicsResult.names();
-            Set<String> topics = topicListFuture.get();
-            if(topics.contains(topic)){
-                System.out.println("Topic already exists. Topic: " + topic);
-                return;
-            }
-            // topic does not exist
-            CreateTopicsResult result = admin.createTopics(Arrays.asList(new NewTopic(topic, partitions, replicationCount).configs(new HashMap<>())));
-            KafkaFuture<Void> future = result.all();
-            future.get();
-            System.out.println("Created new topic. Topic: " + topic);
-        } catch (InterruptedException | ExecutionException e) {
-            throw new Exception("Error creating topic.", e);
-        }
     }
 }

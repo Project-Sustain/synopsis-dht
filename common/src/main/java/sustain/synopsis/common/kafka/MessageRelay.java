fbb1be6f2ef43bfb592559e37dc7a88c627fc46b
Thilina Buddhika <thilinamb@gmail.com>
2020-02-27 16:01:50 -0700

    Cleaning up some unused Kafka publisher code

--- common/src/main/java/sustain/synopsis/common/kafka/MessageRelay.java
+++
@@ -1,9 +0,0 @@
-package sustain.synopsis.common.kafka;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface MessageRelay<K,V> {
-    public void put(ConsumerRecord<K,V> record);
-
-    public ConsumerRecord<K,V> get();
-}

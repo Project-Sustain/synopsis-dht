20ae9440dfcfcbc43a50cdf282e19b9b5641dc06
Thilina Buddhika <thilinamb@gmail.com>
2020-03-27 22:39:55 -0600

    Service method implementation for ending a session

--- dht/src/main/java/sustain/synopsis/dht/IngestionRequestDispatcher.java
+++ dht/src/main/java/sustain/synopsis/dht/IngestionRequestDispatcher.java
@@ -4,10 +4,7 @@ import sustain.synopsis.dht.store.StorageException;
 import sustain.synopsis.dht.store.StrandStorageKey;
 import sustain.synopsis.dht.store.StrandStorageValue;
 import sustain.synopsis.dht.store.node.NodeStore;
-import sustain.synopsis.dht.store.services.IngestionRequest;
-import sustain.synopsis.dht.store.services.IngestionResponse;
-import sustain.synopsis.dht.store.services.NodeMapping;
-import sustain.synopsis.dht.store.services.Strand;
+import sustain.synopsis.dht.store.services.*;
 import sustain.synopsis.dht.store.workers.WriterPool;
 
 import java.io.IOException;
@@ -25,16 +22,12 @@ public class IngestionRequestDispatcher {
         this.writerPool = new WriterPool(Context.getInstance().getNodeConfig().getWriterPoolSize());
     }
 
-    public CompletableFuture<NodeMapping> getNodeMappingCompletableFuture(String datasetId, long sessionId, Strand strand) {
+    public CompletableFuture<NodeMapping> getNodeMappingCompletableFuture(String datasetId, long sessionId,
+                                                                          Strand strand) {
         return CompletableFuture.supplyAsync(() -> {
             try {
-                nodeStore.store(
-                        datasetId,
-                        strand.getEntityId(),
-                        sessionId,
-                        new StrandStorageKey(strand.getFromTs(), strand.getToTs()),
-                        new StrandStorageValue(strand.getBytes().toByteArray())
-                );
+                nodeStore.store(datasetId, strand.getEntityId(), sessionId, new StrandStorageKey(strand.getFromTs(),
+                        strand.getToTs()), new StrandStorageValue(strand.getBytes().toByteArray()));
             } catch (IOException | StorageException e) {
                 e.printStackTrace();
             }
@@ -42,43 +35,29 @@ public class IngestionRequestDispatcher {
         }, writerPool.getExecutor(strand.getEntityId().hashCode()));
     }
 
-    public CompletableFuture<IngestionResponse> toCombinedCompletableFuture(String datasetId, long sessionId, List<CompletableFuture<NodeMapping>> nodeMappingCompletableFutures) {
+    public CompletableFuture<IngestionResponse> toCombinedCompletableFuture(String datasetId, long sessionId,
+                                                                            List<CompletableFuture<NodeMapping>> nodeMappingCompletableFutures) {
         CompletableFuture<List<NodeMapping>> allCompletableFuture =
-                CompletableFuture.allOf(nodeMappingCompletableFutures.toArray(new CompletableFuture[nodeMappingCompletableFutures.size()]))
-                        .thenApply(future -> {
-                            return nodeMappingCompletableFutures.stream()
-                                    .map(CompletableFuture::join)
-                                    .collect(Collectors.toList());
-        });
+                CompletableFuture.allOf(nodeMappingCompletableFutures.toArray(new CompletableFuture[nodeMappingCompletableFutures.size()])).thenApply(future -> nodeMappingCompletableFutures.stream().map(CompletableFuture::join).collect(Collectors.toList()));
 
-        return allCompletableFuture.thenApply(nodeMappings -> {
-            return IngestionResponse.newBuilder()
-                    .setDatasetId(datasetId)
-                    .setSessionId(sessionId)
-                    .setStatus(true)
-                    .addAllMapping(nodeMappings.stream()
-                            .filter(mapping -> mapping.getDhtNodeAddress() != null)
-                            .collect(Collectors.toList())
-                    )
-                    .build();
-        });
+        return allCompletableFuture.thenApply(nodeMappings -> IngestionResponse.newBuilder().setDatasetId(datasetId).setSessionId(sessionId).setStatus(true).addAllMapping(nodeMappings.stream().filter(mapping -> mapping.getDhtNodeAddress() != null).collect(Collectors.toList())).build());
     }
 
     public CompletableFuture<IngestionResponse> dispatch(IngestionRequest ingestionRequest) {
-        List<CompletableFuture<NodeMapping>> nodeMappingCompletableFutures = new ArrayList<>(ingestionRequest.getStrandCount());
-        for (int i = 0; i < nodeMappingCompletableFutures.size(); i++) {
-            nodeMappingCompletableFutures.set(i, getNodeMappingCompletableFuture(
-                    ingestionRequest.getDatasetId(),
-                    ingestionRequest.getSessionId(),
-                    ingestionRequest.getStrand(i))
-            );
+        List<CompletableFuture<NodeMapping>> nodeMappingCompletableFutures =
+                new ArrayList<>(ingestionRequest.getStrandCount());
+        for (int i = 0; i < ingestionRequest.getStrandCount(); i++) {
+            nodeMappingCompletableFutures.add(i, getNodeMappingCompletableFuture(ingestionRequest.getDatasetId(),
+                    ingestionRequest.getSessionId(), ingestionRequest.getStrand(i)));
         }
 
-        return toCombinedCompletableFuture(
-                ingestionRequest.getDatasetId(),
-                ingestionRequest.getSessionId(),
-                nodeMappingCompletableFutures
-        );
+        return toCombinedCompletableFuture(ingestionRequest.getDatasetId(), ingestionRequest.getSessionId(),
+                nodeMappingCompletableFutures);
     }
 
+    public CompletableFuture<Boolean> terminateSession(TerminateSessionRequest terminateSessionRequest) {
+        List<CompletableFuture<Boolean>> futures = nodeStore.endSession(terminateSessionRequest.getDatasetId(),
+                terminateSessionRequest.getSessionId(), writerPool);
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).thenApply(future -> futures.stream().map(CompletableFuture::join).reduce(true, (b1, b2) -> b1 && b2));
+    }
 }

fcc4cda06c2ebd7a5d4526e7ce7e87ae4f6c95aa
keegan <keegan.millard@gmail.com>
2020-06-30 04:55:58 -0600

    fixed args

--- samples/src/main/java/sustain/synopsis/samples/client/usgs/StreamFlowClient.java
+++ samples/src/main/java/sustain/synopsis/samples/client/usgs/StreamFlowClient.java
@@ -1,12 +1,15 @@
 package sustain.synopsis.samples.client.usgs;
 
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
 import org.apache.log4j.Logger;
-import sustain.synopsis.common.Strand;
 import sustain.synopsis.ingestion.client.core.*;
 import sustain.synopsis.ingestion.client.publishing.DHTStrandPublisher;
-import sustain.synopsis.ingestion.client.publishing.StrandPublisher;
+import sustain.synopsis.metadata.*;
+import sustain.synopsis.metadata.MetadataServiceGrpc.MetadataServiceBlockingStub;
 import sustain.synopsis.samples.client.common.Location;
 import sustain.synopsis.samples.client.common.State;
+import sustain.synopsis.sketch.dataset.Quantizer;
 
 import java.io.*;
 import java.time.Duration;
@@ -29,6 +32,7 @@ public class StreamFlowClient {
     static String dhtNodeAddress;
     static String datasetId;
     static Map<String, Location> stationMap;
+    static MetadataServiceBlockingStub metadataStub;
 
     static void ingest(List<File> allFiles, String stateAbbr, int year, long sessionId) {
         String key = stateAbbr+year;
@@ -44,9 +48,21 @@ public class StreamFlowClient {
                 .filter(new UsgsUtil.FilePredicate(stateAbbr, year, year))
                 .sorted().collect(Collectors.toList());
 
-        SessionSchema sessionSchema = new SessionSchema(Util.quantizerMapFromString(binConfig), GEOHASH_LENGTH, TEMPORAL_BRACKET_LENGTH);
+        Map<String, Quantizer> quantizerMap = Util.quantizerMapFromString(binConfig);
+
+        SessionSchema sessionSchema = new SessionSchema(quantizerMap, GEOHASH_LENGTH, TEMPORAL_BRACKET_LENGTH);
         DHTStrandPublisher publisher = new DHTStrandPublisher(dhtNodeAddress, datasetId, sessionId);
-//        StrandPublisher publisher = new MyStrandPublisher();
+
+        PublishMetadataResponse publishMetadataResponse = metadataStub.publishMetadata(PublishMetadataRequest.newBuilder()
+                .setDatasetId(datasetId)
+                .setSessionMetadata(ProtoBuffSerializedSessionMetadata.newBuilder()
+                        .setSessionId(sessionId)
+                        .addBinConfiguration(serializedBinConfiguration(DISCHARGE_FEATURE, quantizerMap.get(DISCHARGE_FEATURE)))
+                        .build()
+                ).build()
+        );
+
+        logger.info("publish metadata response for sessionId "+sessionId+": "+publishMetadataResponse.getStatus());
 
         StrandRegistry strandRegistry = new StrandRegistry(publisher, 10000, 100);
         TemporalQuantizer temporalQuantizer = new TemporalQuantizer(TEMPORAL_BRACKET_LENGTH);
@@ -68,6 +84,13 @@ public class StreamFlowClient {
         logger.info("Total strands: "+publisher.getStrandsPublishedCount());
     }
 
+    static ProtoBuffSerializedBinConfiguration serializedBinConfiguration(String featureName, Quantizer binConfiguration) {
+        return ProtoBuffSerializedBinConfiguration.newBuilder()
+                .setFeatureName(featureName)
+                .addAllValues(binConfiguration.getTicks().stream().map(f -> f.getFloat()).collect(Collectors.toList()))
+                .build();
+    }
+
     static Map<String, String> getBinConfigMap(File f) throws IOException {
         Map<String, String> ret = new HashMap<>();
         try (BufferedReader br = new BufferedReader(new FileReader(f))) {
@@ -83,21 +106,36 @@ public class StreamFlowClient {
         return ret;
     }
 
+    static MetadataServiceBlockingStub getMetadataStubForAddress(String metadataAddress) {
+        String[] splits  = metadataAddress.split(":");
+        String host = splits[0];
+        int port = Integer.parseInt(splits[1]);
+
+        Channel channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        MetadataServiceBlockingStub stub = MetadataServiceGrpc.newBlockingStub(channel);
+        return stub;
+    }
+
     public static void main(String[] args) throws IOException {
         File inputDir = new File(args[0]);
         stationMap = StationParser.parseFile(new File(args[1]));
         binConfigMap = getBinConfigMap(new File(args[2]));
         dhtNodeAddress = args[3];
-        datasetId = args[4];
-        int beginYear = Integer.parseInt(args[5].substring(0,4));
-        int endYear =  Integer.parseInt(args[6].substring(0,4));
+        metadataStub = getMetadataStubForAddress(args[4]);
+        datasetId = args[5];
+        int beginYear = Integer.parseInt(args[6].substring(0,4));
+        int endYear =  Integer.parseInt(args[7].substring(0,4));
         List<File> allFiles = sustain.synopsis.samples.client.common.Util.getFilesRecursive(inputDir, 0);
 
         long sessionId = 0;
         for (State s : State.values()) {
             String stateAbbr = s.getANSIAbbreviation().toLowerCase();
             for (int year = beginYear; year <= endYear; year++) {
-                ingest(allFiles, stateAbbr, year, sessionId++);
+                try {
+                    ingest(allFiles, stateAbbr, year, sessionId++);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         }
 
